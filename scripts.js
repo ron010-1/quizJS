ab534d1f4340e00cd36db1f194f31c05bee56eb2
Riquelme Oliveira <riquuelmelive@gmail.com>
2024-08-15 21:28:09 -0300

    bug: restart game

--- scripts.js
+++ scripts.js
@@ -7,62 +7,61 @@ let trueOrFalse = document.getElementById("trueOrFalse");
 let questionsCounter = 0;
 let trueAnswers = 0;
 
-async function readQuestions(){
+async function readQuestions() {
     const data = await fetch('./questions.json'); //Pega todos os dados usando assincronismo e armazena em data
     const dados = await data.json();//Converte a data em JSON que é o objeto padrão do javaScript
     return dados; //retorna um array
 }
 
-function generateNumber(){
+function generateNumber() {
     return Math.floor(Math.random() * 50) + 1;
 }
 
 //Mandar id.questao pro localStorage
 
-function addQuestion(question){
+function addQuestion(question) {
     trueOrFalse.innerText = "";
     questionsCounter++;
     let p = document.createElement('p');
     p.id = 'text'
     question.alternatives.forEach((alternative, index) => {
-        let buttons = [];
         let button = document.createElement('button');
         button.id = 'alternatives'
         button.innerText = question.alternatives[index];
         divAnswers.appendChild(button);
-        buttons += button;
         button.addEventListener('click', () => {
-            if(question.alternatives[index] === question.answer){
+            if (question.alternatives[index] === question.answer) {
                 button.style.backgroundColor = '#9aeabc';
                 trueOrFalse.innerText = "Você acertou!";
                 trueAnswers++;
-            }else{
+            } else {
                 button.style.backgroundColor = '#ff9393';
                 trueOrFalse.innerText = "Você errou!";
             }
-            localStorage.setItem("id",() => {
+            localStorage.setItem("id", () => {
                 const local = localStorage.getItem("id");
-                if(local !== null){
+                if (local !== null) {
                     localStorage.setItem("id", JSON.stringify([...JSON.parse(local), question.id]))
                 }
             });
-            
+
         })
     });
     p.innerText = question.text;
     divQuestion.appendChild(p);
-    if(questionsCounter === 5){
+    if (questionsCounter === 5) {
         questionsDiv.style.display = "none";
         startDiv.style.display = "flex";
         window.alert("Você acertou " + trueAnswers + " perguntas");
         questionsCounter = 0;
         trueAnswers = 0;
+        cleanSection();
     }
 }
 
-function generateQuestions(questions){
+function generateQuestions(questions) {
     let i = 1;
-    while(i){
+    while (i) {
         let numeroAleatorio = generateNumber();
         let question = questions[numeroAleatorio];
         addQuestion(question);
@@ -70,17 +69,21 @@ function generateQuestions(questions){
     }
 }
 
-async function regenerateQuestion() {
+function cleanSection(){
     // Seleciona todos os botões com o id 'alternatives' e remove cada um deles
     const buttons = document.querySelectorAll('#alternatives');
-    
     buttons.forEach(button => button.remove()); // Remove cada botão encontrado
-    
+    // Limpa o texto da pergunta
+    divQuestion.innerHTML = '';
+}
+
+async function regenerateQuestion() {
+    cleanSection();
     // Limpa o texto da pergunta
     if (divQuestion) {
         divQuestion.innerHTML = '';
     }
-    
+
     const dados = await readQuestions();
     generateQuestions(dados);
 }
